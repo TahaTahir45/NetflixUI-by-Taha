3185f5752dc31d4c7efacc371ca1dd38bcd64c03
taha <tahatahir996@gmail.com>
2024-06-17 23:35:00 +0500

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Taha Tahir on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
